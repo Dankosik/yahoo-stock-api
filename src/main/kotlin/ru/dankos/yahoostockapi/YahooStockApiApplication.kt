8a90396e0899b62eef284adf203f82f0b1122cf2
Koryto Daniil <DKoryto@alfabank.ru>
2022-08-13 21:42:59 +1000

    enabled CaffeineProperties

--- src/main/kotlin/ru/dankos/yahoostockapi/YahooStockApiApplication.kt
+++ src/main/kotlin/ru/dankos/yahoostockapi/YahooStockApiApplication.kt
@@ -1,13 +1,18 @@
 package ru.dankos.yahoostockapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 import reactivefeign.spring.config.EnableReactiveFeignClients
+import ru.dankos.yahoostockapi.config.CaffeineProperties
 
 @EnableCaching
 @SpringBootApplication
 @EnableReactiveFeignClients
+@EnableConfigurationProperties(
+    CaffeineProperties::class
+)
 class YahooStockApiApplication
 
 fun main(args: Array<String>) {
